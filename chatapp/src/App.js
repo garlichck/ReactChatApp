6d32387da068f3e3f5ce71856db5cd0737257ce5
garlichck <garlichck@hotmail.com>
2022-01-20 17:04:50 +0800

    [ReactChatApp]-Import React Chat Engine.

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { ChatEngine } from "react-chat-engine";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellow World HCK</h1>
+      <ChatEngine height="100vh" 
+      projectID="c90e2720-25f1-4494-ae87-3031781d69c0"
+      userName="cheekong"
+      userSecret="abc123">
+
+
+      </ChatEngine>
     </div>
   );
 }
