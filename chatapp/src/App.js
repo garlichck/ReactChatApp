78e96f31e9539bd24dce6fc5110a0bf886aac742
garlichck <garlichck@hotmail.com>
2022-01-24 16:28:41 +0800

    [ReactChatApp]-Implement chat form and login form.

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,21 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
-import { ChatEngine } from "react-chat-engine";
-import ChatFeed from "./components/ChatFeed.jsx";
 
-function App() {
+const projectID = 'c90e2720-25f1-4494-ae87-3031781d69c0';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-    <div className="App">
-      <ChatEngine height="100vh" 
-      projectID="c90e2720-25f1-4494-ae87-3031781d69c0"
-      userName="cheekong"
-      userSecret="abc123"
-      renderChatFeed= { (chatAppProps) => <ChatFeed  {...chatAppProps}/> }
-      onNewMessage ={ () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play }>
-      
-
-      </ChatEngine>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
 
 export default App;
